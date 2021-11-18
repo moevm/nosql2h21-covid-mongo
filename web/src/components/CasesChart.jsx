63879cb5c06ccdc002bb36e7045cf1901363e1bf
John C. Alastor <kinekt2000@gmail.com>
2021-11-18 18:31:03 +0300

    pretty vaccinations and cases charts

--- web/src/components/CasesChart.jsx
+++ web/src/components/CasesChart.jsx
@@ -3,16 +3,13 @@ import CircularProgress from '@mui/material/CircularProgress'
 import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography'
-import { Line } from 'react-chartjs-2';
 
 import { useEffect } from 'react';
 
 import useFetch from 'hooks/useFetch';
 import { CASES_PER_DAY } from 'api/endpoints';
 
-function int(value) {
-  return ~~value
-}
+import WorldChart from 'components/WorldChart';
 
 const CasesChart = () => {
   const [cases, performCasesFetch] = useFetch(CASES_PER_DAY)
@@ -36,96 +33,13 @@ const CasesChart = () => {
 
   const chart = (response) => 
   {
-    const data = response.data;
+    const data = response.data.map(item => ({
+      date: item.date,
+      value: item.new_cases,
+      value_smoothed: item.new_cases_smoothed
+    }));
     return (
-      <Line
-        data={{
-          datasets: [{
-            label: "New cases",
-            data: data.map(item => ({x: item.date, y: item.new_cases})),
-            borderColor: "red",
-            fill: "origin",
-            backgroundColor: "yellow",
-            pointRadius: 0,
-            hoverRadius: 0,
-            showLine: true,
-            order: 1
-          },{
-            label: "7-day avg",
-            data: data.map(item => ({x: item.date, y: int(item.new_cases_smoothed)})),
-            borderColor: "green",
-            pointRadius: 0,
-            hoverBackgroundColor: "green",
-            showLine: true,
-            order: 0
-          }]
-        }}
-        options={{
-          maintainAspectRatio: false,
-          scales: {
-            xAxes: {
-              ticks: {
-                autoSkip: true,
-                maxTicksLimit: 10
-              }
-            }
-          },
-          mouseLine: {
-            color: "magenta"
-          },
-          interaction: {
-            intersect: false,
-            mode: "index"
-          },
-          plugins: {
-            legend: {
-              display: false
-            },
-            tooltip: {
-              displayColors: false,
-              callbacks: {
-                afterTitle: (items) => {items.reverse()}
-              }
-            },
-          }
-        }}
-        plugins={[
-          {
-            id: 'mouseLine',
-            afterEvent: function (chart, {event: e}) {
-              var chartArea = chart.chartArea;
-              if (
-                e.x >= chartArea.left &&
-                e.y >= chartArea.top &&
-                e.x <= chartArea.right &&
-                e.y <= chartArea.bottom && 
-                chart._active.length
-              ) {
-                chart.options.mouseLine.x = chart._active[0].element.x;
-              } else {
-                chart.options.mouseLine.x = NaN;
-              }
-            },
-            afterDraw: function (chart, _) {
-              var ctx = chart.ctx;
-              var chartArea = chart.chartArea;
-              var x = chart.options.mouseLine.x;
-      
-              if (!isNaN(x)) {
-                ctx.save();
-                ctx.strokeStyle = chart.options.mouseLine.color;
-                ctx.setLineDash([5, 10])
-                ctx.lineWidth = 1
-                ctx.moveTo(chart.options.mouseLine.x, chartArea.bottom);
-                ctx.lineTo(chart.options.mouseLine.x, chartArea.top);
-                ctx.stroke();
-                ctx.restore();
-              }
-            }
-          }
-        ]}
-        width="100%"
-      />
+      <WorldChart data={data} label="New cases" smoothedLabel="7-day avg" primaryColor="#174ea6" secondaryColor="#8ab4f8"/>
     )
   }
 
---
+++ web/src/components/WorldChart.jsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import { Chart } from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+import Color from 'color';
+
+const setupDataset = (data, label, color) => ({
+  label,
+  data,
+  borderColor: color.toString(),
+  fill: "origin",
+  backgroundColor: color.fade(0.2).toString(),
+  pointRadius: 0,
+  hoverRadius: 0,
+  showLine: true,
+})
+
+const setupSmoothedDataset = (data, label, color) => ({
+  label,
+  data,
+  borderColor: color.toString(),
+  pointRadius: 0,
+  hoverBackgroundColor: color.toString(),
+  showLine: true,
+})
+
+const chartOptions = {
+  maintainAspectRatio: false,
+  scales: {
+    xAxes: {
+      ticks: {
+        autoSkip: true,
+        maxTicksLimit: 10
+      }
+    }
+  },
+  mouseLine: {
+    color: "#333"
+  },
+  interaction: {
+    intersect: false,
+    mode: "index"
+  },
+  plugins: {
+    legend: {
+      display: false
+    },
+    tooltip: {
+      displayColors: false,
+      position: "custom",
+      callbacks: {
+        afterTitle: (items) => {items.reverse()}
+      }
+    },
+  }
+}
+
+const verticalLinePlugin = {
+  id: 'mouseLine',
+  afterEvent: function (chart, {event: e}) {
+    var chartArea = chart.chartArea;
+    if (
+      e.x >= chartArea.left &&
+      e.y >= chartArea.top &&
+      e.x <= chartArea.right &&
+      e.y <= chartArea.bottom && 
+      chart._active.length
+    ) {
+      chart.options.mouseLine.x = chart._active[0].element.x;
+    } else {
+      chart.options.mouseLine.x = NaN;
+    }
+  },
+  beforeDatasetDraw: function (chart, {index, ...other}) {
+    if (index !== 0) return
+
+    var ctx = chart.ctx;
+    var chartArea = chart.chartArea;
+    var x = chart.options.mouseLine.x;
+
+    if (!isNaN(x)) {
+      ctx.save();
+      
+      ctx.strokeStyle = chart.options.mouseLine.color;
+      ctx.setLineDash([5, 10])
+      ctx.lineWidth = 1
+
+      ctx.beginPath();
+      ctx.moveTo(x, chartArea.bottom);
+      ctx.lineTo(x, chartArea.top);
+      ctx.stroke();
+
+      ctx.restore();
+    }
+  }
+}
+
+const tooltipPlugin = Chart.registry.getPlugin('tooltip');
+tooltipPlugin.positioners.custom = function(items) {
+  if (items.length) {
+    return {
+      x: items[0].element.x,
+      y: items[0].element.y,
+    };
+  }
+  return false
+}
+
+const int = value => ~~value
+
+const WorldChart = ({data, label="data", smoothedLabel="smoothed data", primaryColor="#737373", secondaryColor="#4d4d4d"}) => {
+  primaryColor = Color(primaryColor)
+  secondaryColor = Color(secondaryColor)
+
+  return (
+      <Line
+      data={{
+        datasets: [
+          setupSmoothedDataset(
+            data.map(item => ({x: item.date, y: int(item.value_smoothed)})),
+            smoothedLabel, secondaryColor
+          ),
+          setupDataset(
+            data.map(item => ({x: item.date, y: item.value})),
+            label, primaryColor
+          ),
+        ]
+      }}
+      options={chartOptions}
+      plugins={[verticalLinePlugin]}
+      width="100%"
+    />
+  )
+}
+
+export default WorldChart
